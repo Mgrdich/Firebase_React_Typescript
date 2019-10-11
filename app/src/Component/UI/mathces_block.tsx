1a7ea6d9efbc07d2ea0e962d9c683209313d5d10
Mgrdich <mgotm13@gmail.com>
2019-10-11 23:49:22 +0400

    Matches Fetching

---
+++ app/src/Component/UI/mathces_block.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const MathcesBlock = (props: any) => {
+    const {match} = props;
+    return (
+        <div className="match_block">
+            <div className="match_date">
+                {
+                    match.final ? match.date : `Match is not played yet: ${match.date}`
+                }
+            </div>
+            <div className="match_wrapper">
+                <div className="match_top">
+                    <div className="left">
+                        <div className="icon" style={{
+                            background: `url(/Style/images/team_icons/${match.localThmp})`
+                        }}/>
+                        <div className="team_name">{match.local}</div>
+                    </div>
+                    <div className="right">
+                        {match.final ? match.resultLocal : '-'}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MathcesBlock;
--- app/src/Component/home/matches/blocks.tsx
+++ app/src/Component/home/matches/blocks.tsx
@@ -2,25 +2,31 @@ import React, {useEffect, useState} from 'react';
 import {firebaseMatches} from "../../../Firebase";
 import {firebaseLooper} from "../../../utilities/funcions";
 import ClipLoader from 'react-spinners/ClipLoader';
+import MathcesBlock from "../../UI/mathces_block";
 
 const Blocks = () => {
-    const [matches, changeMatches] = useState();
+    const [matches, changeMatches] = useState<Array<any>>([]);
     useEffect(() => {
         firebaseMatches.limitToLast(6).once('value').then((snapshot) => {
-            changeMatches(firebaseLooper(snapshot));
+            changeMatches(firebaseLooper(snapshot).reverse());
         })
     }, []);
-    const showMatches = function (): JSX.Element {
-        return (
-            <div>
-                Match
-            </div>
-        );
+    const showMatches = function (matches: any): JSX.Element {
+        return matches.map((match:any) => {
+            return (
+                <div className="item">
+                    <div className="wrapper">
+                        <MathcesBlock match={match}/>
+                    </div>
+                </div>
+            );
+
+        });
     };
 
     return (
         <div>
-            {(matches.length) ? showMatches() : <ClipLoader color="#0d1831" size={150} sizeUnit="px"/>}
+            {(matches.length) ? showMatches(matches) : <ClipLoader color="#0d1831" size={150} sizeUnit="px"/>}
         </div>
     );
 };
