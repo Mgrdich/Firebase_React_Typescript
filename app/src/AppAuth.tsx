3d31d48241ef53f7cbd03bfddd20711913e36d8b
Mgrdich <mgotm13@gmail.com>
2019-12-01 23:41:43 +0400

    minor modifications

--- app/src/AppAuth.tsx
+++ app/src/AppAuth.tsx
@@ -9,12 +9,13 @@ export const UserContext = React.createContext<any>({
 
 const AppAuth = () => {
     const {initializing, user} = useAuth();
+
     if (initializing) {
         return <ClipLoader color="#0d1831" size={450} sizeUnit="px"/>;
     }
 
     return (
-        <UserContext.Provider value={{user}}>
+        <UserContext.Provider value={{user,initializing}}>
             <App/>
         </UserContext.Provider>
     );
--- app/src/reusableHooks/useSession.ts
+++ app/src/reusableHooks/useSession.ts
@@ -2,6 +2,6 @@ import  {useContext} from "react";
 import {UserContext} from "../AppAuth";
 
 export const useSession = () => {
-    const {user} = useContext(UserContext);
-    return {user: user};
+    const {user,initializing} = useContext(UserContext);
+    return {user,initializing};
 };
